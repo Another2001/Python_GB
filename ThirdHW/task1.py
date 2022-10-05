446ecd9bbff843ea21f3a5ffa5eeccb5ecfe267d
A_not_her <sar.den.art2001@ya.ru>
2022-10-05 15:08:05 +0800

    решена 1 задача 3 дз

--- ThirdHW/task1.py
+++ ThirdHW/task1.py
@@ -1,19 +1,15 @@
-N = input('Задайте кол-во элементов списка: ')
-while N.isdigit() == False:
-    print('Введено не число! повторите попытку ввода!')
-    N = input()
-else:
-    stri = ''
-    sum = 0
-    N = int(N)
-    lis = list()
-    for i in range(N):
-        lis.insert(i, input('Введите элемент списка(число) --> '))
-        lis[i] = int(lis[i])
-        if (i-1) % 2 == 1:                #так визуальнее более правильно :)
-            stri += f" {lis[i]} , "
-            sum += lis[i]
-    print(f"Готовый список: {lis} , на нечётных позициях элементы - {stri} их сумма: {sum}")
+from CheGit import checkdigit
+N = checkdigit(input('укажите кол-во элементов списка -->'))
+sum = 0
+stri = ''
+lis = list()
+for i in range(N):
+    lis.insert(i, checkdigit(input('Введите элемент списка(число) --> ')))
+    lis[i] = int(lis[i])
+    if (i-1) % 2 == 1:                #так визуальнее более правильно :)
+        stri += f" {lis[i]} , "
+        sum += lis[i]
+print(f"Готовый список: {lis} , на нечётных позициях элементы - {stri} их сумма: {sum}")
 
 
 
