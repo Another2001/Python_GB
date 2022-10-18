a04b9b01039a43c54cd4f6e9c0c950f1829c5acc
A_not_her <sar.den.art2001@ya.ru>
2022-10-18 15:37:21 +0800

    .

--- SixHW/task1.py
+++ SixHW/task1.py
@@ -3,9 +3,9 @@
 text = 'Напишите абв напиабв програбвмму программу, удаляющую из \
     этого абв текста все вабвс слова, содерабващие содержащие "абв"'
 
-def clearing_text(text):
+def clr_txt(text):
     text = list(filter(lambda x: 'абв' not in x, text.split()))
     return " ".join(text)
 
-text = clearing_text(text)
+text = clr_txt(text)
 print(text)
